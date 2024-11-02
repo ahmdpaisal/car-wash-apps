65dad662c8bd200ef37ea01fa51a294b60e01560
ahmdpaisal <ahmadpaisal118@gmail.com>
2024-11-02 07:51:26 +0700

    Update: disable CreateAction in employee earning

--- app/Filament/Resources/EmployeeEarningResource/Pages/ManageEmployeeEarnings.php
+++ app/Filament/Resources/EmployeeEarningResource/Pages/ManageEmployeeEarnings.php
@@ -15,7 +15,7 @@ class ManageEmployeeEarnings extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 
